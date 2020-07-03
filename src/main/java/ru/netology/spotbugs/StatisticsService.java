1193cfbd9a6b671d9094a3907527a24d871e6ae5
Vera Var <primavera1919@yandex.ru>
2020-07-03 17:41:11 +0300

    Initial commit

---
+++ src/main/java/ru/netology/spotbugs/StatisticsService.java
@@ -0,0 +1,35 @@
+package ru.netology.spotbugs;
+
+public class StatisticsService {
+
+    public long findMax(long[] incomes) {
+        long current_max_index = 0;
+        long current_max = incomes[0];
+        for (long income : incomes)
+            if (current_max < income)
+                current_max = income;
+        return current_max;
+    }
+
+    public long findMin(long[] incomes) {
+        long current_min_index = 0;
+        long current_min = incomes[0];
+        for (long income : incomes)
+            if (current_min > income)
+                current_min = income;
+        return current_min;
+    }
+
+    public long findSum(long[] incomes){
+        long sum = 0;
+        for (long income : incomes) {
+            sum += income;
+        }
+        return sum;
+    }
+
+    public long findAverage(long[] incomes){
+        long average = findSum(incomes) / incomes.length;
+        return average;
+    }
+}
---
+++ src/test/java/ru/netology/spotbugs/StatisticsServiceTest.java
@@ -0,0 +1,39 @@
+package ru.netology.spotbugs;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StatisticsServiceTest {
+
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+
+        @Test
+        void shouldFindMax(){
+            long expected = 12;
+            long actual = service.findMax(incomesInBillions);
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        void shouldFindMin(){
+            long expected = 3;
+            long actual = service.findMin(incomesInBillions);
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        void shouldFindSum(){
+            long expected = 85;
+            long actual = service.findSum(incomesInBillions);
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        void shouldFindAverage(){
+            long expected = 7;
+            long actual = service.findAverage(incomesInBillions);
+            assertEquals(expected, actual);
+        }
+}
